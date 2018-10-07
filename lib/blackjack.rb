85ad25326388b3695be8c2f9850e25deae5544fb
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-07 00:17:59 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -38,17 +38,19 @@ end
 
 def hit?(card_total)
   # code hit? here
+  x = 0
   prompt_user
   user_input = get_user_input
   case user_input.downcase
   when "s"
     card_total
   when "h"
-    card_total += deal_card
-    binding.pry
+    x += deal_card
   else
     invalid_command
   end
+  card_total += x
+  binding.pry
 end
 
 def invalid_command
